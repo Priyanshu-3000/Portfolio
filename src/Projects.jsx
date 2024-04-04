2e715bd7bda29d5a8ec049dd6807811673601c0b
Priyanshu <sainipriyanshu59@gmail.com>
2024-04-05 00:48:27 +0530

    edit project section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -22,14 +22,14 @@ function Project() {
     {
       img: "https://imgs.search.brave.com/yOpRp52oUm3Bb3KQ39kEA__KYYRkH7qI92JhA83pN9c/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNS8w/Ny8wNS8xMC8xOC90/cmVlLTgzMjA3OV82/NDAuanBn",
       title: "NEWS WEBSITE",
-      techstack: "laravel",
+      techstack: "React , Express , Node , Mongodb",
       discription:
-        "qwerthdjfad adugisd fh ahfyweihfhajs  dfh uf ghruiefrhqwr yg yd",
+        "",
     },
     {
       img: "https://imgs.search.brave.com/Kl7Nigk3nHo0df8uaBabYa77iPZBJos7G09ySBfFnRU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE0/NDc3MTA0NDE2MDQt/NWJkYzQxYmM2NTE3/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OGNH/bGpkSFZ5Wlh4bGJu/d3dmSHd3Zkh4OE1B/PT0",
-      title: "E-COMMERCE",
-      techstack: "laravel",
+      title: "Portfolio",
+      techstack: "React , Bootstrap",
       discription: "",
     },
     {
